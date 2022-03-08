e8784f9ab8188944fe96ab62650437729a1c4067
Vinicius <vini.evangelista@live.com>
2022-03-07 21:08:51 -0300

    feat: Adiciona Botão na Login

--- flutter_app_workskills/flutter_application_1/lib/main.dart
+++ flutter_app_workskills/flutter_application_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +12,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    const title = 'Flutter Demo';
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text(title),
-        ),
-        body: const MyForm(),
+        body: MyForm(),
       ),
     );
   }
@@ -35,27 +32,39 @@ class MyForm extends StatelessWidget {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Email',
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 50, vertical: 100),
+              child: SizedBox(
+                width: 320,
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                  ),
+                ),
+              )),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 0),
+              child: SizedBox(
+                width: 320,
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Senha',
+                  ),
+                ),
+              )),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 100, horizontal: 50),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(fixedSize: const Size(320, 40)),
+              onPressed: () {},
+              child: const Text('Acessar'),
             ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Senha',
-            ),
-          ),
-        ),
-      ],
-    );
+          )
+        ]);
   }
 }
